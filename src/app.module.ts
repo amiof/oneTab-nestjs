aacee75c789f7df8262221834babbbadfa14c612
amiof <arm131313@gmail.com>
2023-07-03 15:17:10 +0330

    add jwt and passport and bcryptjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { databasePorvider } from './database.provider';
+import { PassportModule } from "@nestjs/passport"
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ...databasePorvider],
+  imports: [UsersModule, ...databasePorvider,PassportModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
