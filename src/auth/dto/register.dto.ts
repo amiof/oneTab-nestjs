ad58d8513d8c2433bee7de9e55717b154ac7e698
amiof <arm131313@gmail.com>
2023-07-03 15:27:50 +0330

    add dto for login and register

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsString, IsNotEmpty, IsNumber, IsOptional } from "class-validator"
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly userName: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly first_name: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly last_name: string;
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly password: string;
+  @IsNumber()
+  @IsOptional()
+  readonly age: number;
+}
