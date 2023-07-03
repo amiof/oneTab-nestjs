f23385ea256cfd537d1fca218c218c47cf392b83
amiof <arm131313@gmail.com>
2023-07-03 12:32:12 +0330

    add validation in user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,20 @@
+import { IsEmail, IsString, IsNotEmpty, IsNumber } from "class-validator"
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   readonly userName: string;
-  readonly firstname: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly first_name: string;
+  @IsNotEmpty()
+  @IsString()
   readonly last_name: string;
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+  @IsNotEmpty()
+  @IsString()
   readonly password: string;
+  @IsNumber()
   readonly age: number;
 }
