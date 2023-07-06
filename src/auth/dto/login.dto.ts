db49410c4ae3fbeb61ed9619ff04a0bd2ad3b011
amiof <arm131313@gmail.com>
2023-07-06 15:43:18 +0330

    swagger for lgoin.dto.ts

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
 export class LoginDto {
+  @ApiProperty({example:"amir",description:"import userName "})
   @IsString()
   @IsNotEmpty()
   userName: string
 
+  @ApiProperty({example:"12345",description:"import userName ",format:"string",})
   @IsNotEmpty()
   @IsString()
   password: string
