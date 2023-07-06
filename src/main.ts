c26da5cbcc4c6e198f3f512a719b5220786c2eee
amiof <arm131313@gmail.com>
2023-07-06 15:22:36 +0330

    add and config swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }))
+  const config = new DocumentBuilder().setTitle("api nestjs").setDescription("one-tabs api").setVersion("1.0").build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("api-doc", app, document)
   await app.listen(3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,37 +2,50 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Users")
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
-
+  @ApiOperation({ summary: 'Create user' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: CreateUserDto,
+  })
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.createUser(createUserDto);
   }
 
+  @ApiOperation({ summary: 'find All users' })
   @Get()
   async findAll() {
     return await this.usersService.findAll();
   }
 
+  @ApiOperation({ summary: 'find user by email' })
   @Post("findByEmail")
   async findUserByEmail(@Body() email: string) {
     return await this.usersService.findUserByEmail(email);
   }
 
+  @ApiOperation({ summary: 'find user by id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
 
+  @ApiOperation({ summary: 'update user by id' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @ApiOperation({ summary: 'delete user by id' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
